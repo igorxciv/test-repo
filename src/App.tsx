ce22174f3fddf3dcb7548dfed35dc4ac63e08aa7
Igor Cheliadinski <63055302+igorepam@users.noreply.github.com>
2021-01-10 17:03:00 -0500

    add my first name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 111
+          Learn React
         </a>
       </header>
     </div>
